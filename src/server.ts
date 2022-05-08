58c93e9f707da8f913d8ff4e2b99e46fe4d3c61c
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-05-07 21:38:48 -0300

    incluindo cors para podermos definir qual front-end podera acessar o back-end

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
+
+// controla quais front-ends podem acessar o back-end
+app.use(cors()); 
 app.use(express.json());
 app.use(routes);
+
 
 app.listen(3333, () => {
   console.log('HTTP server running');
