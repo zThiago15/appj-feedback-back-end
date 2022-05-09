7e36088db892f439edd9033f6cc52767ae3c9517
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-05-09 03:10:37 -0300

    corrigindo erro de imagem

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -42,6 +42,7 @@ export class SubmitFeedbackUserCase {
         `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
         `<p>Tipo do feedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
+        screenshot ? `<img src="${screenshot}" />` : ``,
         `</div>`,
       ].join('\n'),
     });
